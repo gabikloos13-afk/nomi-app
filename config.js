7bf6b0ce3516142c6316e21a86a9046cf35619f0
gabikloos13-afk <gabi.kloos.13@gmail.com>
2026-07-31 20:15:58 +0200

    Update config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+window.NOMI_CONFIG = Object.freeze({
+  appName: 'Nomi',
+
+  appUrl:
+    'https://script.google.com/macros/s/AKfycbxx5jipvmW16yNSonwnSN5yJT_yGrPLpUYsosjufjavyXRI0pLl4Uby9Yb5t7i4o33n/exec',
+
+  githubPagesUrl:
+    'https://gabikloos13-afk.github.io/nomi-app/',
+
+  loadTimeoutMs: 14000
+});
