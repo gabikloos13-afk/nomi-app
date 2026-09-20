405d33f735cf93be0b7ece45bfc9e1433114333a
gabikloos13-afk <gabi.kloos.13@gmail.com>
2026-09-20 14:46:34 +0200

    Fix Nomi PWA config compatibility

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // Apps-Script-Web-App für Nomi 2.
 window.NOMI_CONFIG = Object.freeze({
-  APP_URL: 'https://script.google.com/macros/s/AKfycbxnaiV8o4t7skhkYp6iSNUQSWR_kMku5H2lBkLeXdGcvoORHmGnHgdY13Hwk8YNWiuR4A/exec'
+  APP_URL: 'https://script.google.com/macros/s/AKfycbxnaiV8o4t7skhkYp6iSNUQSWR_kMku5H2lBkLeXdGcvoORHmGnHgdY13Hwk8YNWiuR4A/exec',
+  appUrl: 'https://script.google.com/macros/s/AKfycbxnaiV8o4t7skhkYp6iSNUQSWR_kMku5H2lBkLeXdGcvoORHmGnHgdY13Hwk8YNWiuR4A/exec',
+  loadTimeoutMs: 18000
 });
